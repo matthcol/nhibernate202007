60612fbf8c84ad5f16e549fc4e832e1f37445628
Matthias Colin <matthias.colin@gmail.com>
2020-07-20 11:05:45 +0200

    first mapping corrected

--- MovieApp01/MovieApp01/Program.cs
+++ MovieApp01/MovieApp01/Program.cs
@@ -12,14 +12,23 @@ namespace MovieApp01
 {
     class Program
     {
+        private static ISessionFactory sessionFactory;
         static void Main(string[] args)
         {
+            sessionFactory = Configure();
             AddMovies();
             ReadMovies();
         }
         private static void AddMovies()
         {
-            Movie mGT = new Movie { Title = "Gran Torino", Year = 2008 };
+            using (ISession session = sessionFactory.OpenSession())
+            {
+                ITransaction tx = session.BeginTransaction();
+                Movie mGT = new Movie { Title = "Gran Torino", Year = 2008 };
+                session.Save(mGT);
+                tx.Commit();
+                Console.WriteLine("Movie inserted with id {0:D}", mGT.Id);
+            }
         }
 
         private static void ReadMovies()
@@ -27,12 +36,14 @@ namespace MovieApp01
 
         }
 
-        private ISessionFactory Configure()
+        private static ISessionFactory Configure()
         {
            return Fluently.Configure()
             .Database(
                MsSqlConfiguration.MsSql2012
-                    .ConnectionString("Server = Desktop - RRAODUE\\MSSQL17; Database = dbmovies03; User Id = umovie; Password = password; "))
+                    .ConnectionString("Server=Desktop-RRAODUE\\MSSQL17;Database=dbmovies03;User Id=umovie;Password=password;")
+                    .ShowSql()                    
+                    )
             .Mappings(
                m=>m.FluentMappings.AddFromAssemblyOf<Movie>()
                )
