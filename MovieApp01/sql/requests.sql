6446e25336f6ec0b8157da5ae5aaa9ec7fad47fa
Matthias Colin <matthias.colin@gmail.com>
2020-07-21 13:48:31 +0200

    new requests SQL

--- MovieApp01/sql/requests.sql
+++ MovieApp01/sql/requests.sql
@@ -1,5 +1,22 @@
 use dbmovies02;
 
+select count(*) from Movies;
+select count(*) from Stars;
+select count(*) from Play;
+
 select * from Movies where year = 1992;
+select * from Movies where year <> 1992;
 select * from Movies where year < 1992; -- <=, > ,>=
 select * from Movies where year between 1990 and 1994;
+
+select * from Movies where title like '%star%';
+select * from Movies where title like '%empire%';
+select * from Movies where 	title like '%empire%' or title like '%jedi%';
+select * from Movies where 	title like '%star%' and title like '%jedi%';
+select * from Movies where 	(title like '%empire%' or title like '%jedi%') and genres not like '%Short%';
+select * from Movies where 	title like '%empire%' or (title like '%jedi%' and genres not like '%Short%');
+
+select * from Movies where 	title like '%star%' and year in (1977,1980);
+
+select * from Stars where birthdate is null;
+select * from Stars where birthdate is not null;
